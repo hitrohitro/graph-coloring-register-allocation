cde97bbd4dd0222abb3862060fc0ba3b78f3312e
Ro-hitbk <rohitbk745@gmail.com>
2026-03-22 18:20:44 +0530

    Added reserved tokens

--- ir_parser.py
+++ ir_parser.py
@@ -3,6 +3,28 @@ class Instruction:
         self.defined = defined
         self.used = used if used else []
 
+
+RESERVED_TOKENS = {
+    "load",
+    "store",
+    "mov",
+    "add",
+    "sub",
+    "mul",
+    "div",
+    "use",
+}
+
+
+def is_variable(token):
+    if not token:
+        return False
+    if token.lower() in RESERVED_TOKENS:
+        return False
+    if token.isdigit():
+        return False
+    return token[0].isalpha() or token[0] == "_"
+
 def parse_ir(filename):
     instructions = []
 
@@ -16,15 +38,22 @@ def parse_ir(filename):
             # assignment
             if "=" in tokens:
                 defined = tokens[0]
-                used = tokens[2:]
+                used = [tok for tok in tokens[2:] if is_variable(tok)]
                 instructions.append(Instruction(defined, used))
 
             # use statement
-            elif tokens[0] == "use":
-                instructions.append(Instruction(None, [tokens[1]]))
+            elif tokens[0].lower() == "use":
+                used = [tokens[1]] if len(tokens) > 1 and is_variable(tokens[1]) else []
+                instructions.append(Instruction(None, used))
 
-            # load definition
+            # load definition (e.g. "Load a")
+            elif tokens[0].lower() == "load":
+                defined = tokens[1] if len(tokens) > 1 and is_variable(tokens[1]) else None
+                instructions.append(Instruction(defined, []))
+
+            # single token definition (e.g. "a")
             else:
-                instructions.append(Instruction(tokens[0], []))
+                defined = tokens[0] if len(tokens) == 1 and is_variable(tokens[0]) else None
+                instructions.append(Instruction(defined, []))
 
     return instructions
